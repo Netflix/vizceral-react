80c0e7d524ef55e33b3e1506d502825e8214b9c1
Justin Reynolds <jreynolds@netflix.com>
2016-09-06 10:01:14 -0700

    Point back to dist vizceral

--- src/vizceral.jsx
+++ src/vizceral.jsx
@@ -2,7 +2,7 @@
 
 import { isEqual, some } from 'lodash';
 import React from 'react';
-import VizceralGraph from 'vizceral/src/vizceral.js';
+import VizceralGraph from 'vizceral';
 
 /**
  * ![](https://raw.githubusercontent.com/Netflix/vizceral/master/logo.png)
